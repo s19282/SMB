bc96fb5adddc48ccd3904a08948d8ef02101e633
mateu <s19282@pjwskt.edu.pl>
2022-11-16 12:46:04 +0100

    Create content provider

--- SMB_P01/app/src/main/java/com/example/smb_p01/ProductDao.kt
+++ SMB_P01/app/src/main/java/com/example/smb_p01/ProductDao.kt
@@ -1,5 +1,6 @@
 package com.example.smb_p01
 
+import android.database.Cursor
 import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
@@ -12,18 +13,24 @@ interface ProductDao {
     @Query("SELECT * FROM Product")
     fun getProducts(): LiveData<List<Product>>
 
+    @Query("SELECT * FROM Product")
+    fun getProductsCursor(): Cursor
+
+    @Query("SELECT * FROM Product WHERE id=:id ")
+    fun getProductById(id: Long): Cursor
+
     @Insert
-    fun insert(product: Product)
+    fun insert(product: Product): Long
 
     @Update
-    fun update(product: Product)
+    fun update(product: Product): Int
 
     @Query("SELECT * FROM Product;")
     fun selectAll(): List<Product>
 
     @Query("DELETE FROM Product WHERE product.id = :id")
-    fun delete(id: Long)
+    fun delete(id: Long): Int
 
     @Query("DELETE FROM product")
-    fun deleteAll()
+    fun deleteAll(): Int
 }
