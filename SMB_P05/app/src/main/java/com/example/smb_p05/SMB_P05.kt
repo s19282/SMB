356fced6b8cf9419684999ea91bca82b5320bd95
mateusz <s19282@pjwstk.edu.pl>
2023-01-05 12:05:11 +0100

    Open fav shop list on button click

--- SMB_P05/app/src/main/java/com/example/smb_p05/SMB_P05.kt
+++ SMB_P05/app/src/main/java/com/example/smb_p05/SMB_P05.kt
@@ -7,7 +7,9 @@ import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
+import android.os.Bundle
 import android.widget.RemoteViews
+import androidx.core.content.ContextCompat.startActivity
 
 
 /**
@@ -46,7 +48,7 @@ class SMB_P05 : AppWidgetProvider() {
                 openWebsite
             )
         }
-        if (intent?.action == "com.example.smb_p05.SMB_P05/image") {
+        if (intent?.action == "com.example.smb_p05.SMB_P05_switchImage") {
             val avm = AppWidgetManager.getInstance(context)
             val remoteViews = RemoteViews(context?.packageName, R.layout.s_m_b__p05)
             onOff = if (onOff) {
@@ -60,6 +62,15 @@ class SMB_P05 : AppWidgetProvider() {
                 Integer.parseInt(intent.getStringExtra("id").orEmpty()),
                 remoteViews
             )
+        }
+        if (intent?.action == "com.example.smb_p05.SMB_P05_openFavList" && context != null) {
+            val externalIntent = Intent()
+            externalIntent.component = ComponentName(
+                "com.example.smb_p01",
+                "com.example.smb_p01.FavShopListActivity"
+            )
+            externalIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+            startActivity(context, externalIntent, Bundle())
         }
     }
 }
@@ -81,7 +92,7 @@ internal fun updateAppWidget(
     )
 
     val intentClick2 = Intent()
-    intentClick2.action = "com.example.smb_p05.SMB_P05/image"
+    intentClick2.action = "com.example.smb_p05.SMB_P05_switchImage"
     intentClick2.component = ComponentName(context, SMB_P05::class.java)
     intentClick2.putExtra("id", appWidgetId.toString())
 
@@ -91,6 +102,18 @@ internal fun updateAppWidget(
         intentClick2,
         PendingIntent.FLAG_IMMUTABLE
     )
+    val intentClick3 = Intent()
+    intentClick3.action = "com.example.smb_p05.SMB_P05_openFavList"
+    intentClick3.component = ComponentName(context, SMB_P05::class.java)
+    intentClick3.putExtra("id", appWidgetId.toString())
+
+    val pendingIntent3 = PendingIntent.getBroadcast(
+        context,
+        3,
+        intentClick3,
+        PendingIntent.FLAG_IMMUTABLE
+    )
+    views.setOnClickPendingIntent(R.id.openIntent, pendingIntent3)
     views.setOnClickPendingIntent(R.id.openWebsiteButton, pendingIntent)
     views.setOnClickPendingIntent(R.id.image, pendingIntent2)
     appWidgetManager.updateAppWidget(appWidgetId, views)
