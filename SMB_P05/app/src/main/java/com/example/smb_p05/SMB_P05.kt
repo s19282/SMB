6a38706d68cc86801d593061afdb010d4a23679d
mateusz <s19282@pjwstk.edu.pl>
2023-01-05 22:53:59 +0100

    Add media player

--- SMB_P05/app/src/main/java/com/example/smb_p05/SMB_P05.kt
+++ SMB_P05/app/src/main/java/com/example/smb_p05/SMB_P05.kt
@@ -6,10 +6,13 @@ import android.appwidget.AppWidgetProvider
 import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
+import android.media.MediaPlayer
 import android.net.Uri
 import android.os.Bundle
 import android.widget.RemoteViews
 import androidx.core.content.ContextCompat.startActivity
+import com.example.smb_p05.SMB_P05.Companion.mediaPlayer
+import com.example.smb_p05.SMB_P05.Companion.songId
 
 
 /**
@@ -17,7 +20,11 @@ import androidx.core.content.ContextCompat.startActivity
  */
 class SMB_P05 : AppWidgetProvider() {
     companion object {
-        var onOff = true
+        var imageSwitch = true
+        var musicSwitch = true
+        var playPause = true
+        var songId = R.raw.moda
+        lateinit var mediaPlayer: MediaPlayer
     }
 
     override fun onUpdate(
@@ -41,6 +48,7 @@ class SMB_P05 : AppWidgetProvider() {
 
     override fun onReceive(context: Context?, intent: Intent?) {
         super.onReceive(context, intent)
+
         if (intent?.action == "com.example.smb_p05.SMB_P05") {
             val openWebsite = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.pja.edu.pl/"))
             openWebsite.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
@@ -51,7 +59,7 @@ class SMB_P05 : AppWidgetProvider() {
         if (intent?.action == "com.example.smb_p05.SMB_P05_switchImage") {
             val avm = AppWidgetManager.getInstance(context)
             val remoteViews = RemoteViews(context?.packageName, R.layout.s_m_b__p05)
-            onOff = if (onOff) {
+            imageSwitch = if (imageSwitch) {
                 remoteViews.setImageViewResource(R.id.image, R.drawable.apple)
                 false
             } else {
@@ -62,6 +70,39 @@ class SMB_P05 : AppWidgetProvider() {
                 Integer.parseInt(intent.getStringExtra("id").orEmpty()),
                 remoteViews
             )
+
+        }
+        if (intent?.action == "com.example.smb_p05.SMB_P05_music_playPause" && context != null) {
+            val avm = AppWidgetManager.getInstance(context)
+            val remoteViews = RemoteViews(context.packageName, R.layout.s_m_b__p05)
+            playPause = if (playPause) {
+                remoteViews.setImageViewResource(R.id.playPause, android.R.drawable.ic_media_pause)
+                mediaPlayer.start()
+                false
+            } else {
+                remoteViews.setImageViewResource(R.id.playPause, android.R.drawable.ic_media_play)
+                mediaPlayer.pause()
+                true
+            }
+            avm.updateAppWidget(
+                Integer.parseInt(intent.getStringExtra("id").orEmpty()),
+                remoteViews
+            )
+        }
+        if (intent?.action == "com.example.smb_p05.SMB_P05_music_next" && context != null) {
+            musicSwitch = if (musicSwitch) {
+                songId = R.raw.serpentine
+                false
+            } else {
+                songId = R.raw.moda
+                true
+            }
+            mediaPlayer.release()
+            mediaPlayer = MediaPlayer.create(context, songId)
+        }
+        if (intent?.action == "com.example.smb_p05.SMB_P05_music_stop" && context != null) {
+            mediaPlayer.stop()
+            mediaPlayer = MediaPlayer.create(context, songId)
         }
         if (intent?.action == "com.example.smb_p05.SMB_P05_openFavList" && context != null) {
             val externalIntent = Intent()
@@ -81,6 +122,8 @@ internal fun updateAppWidget(
     appWidgetId: Int
 ) {
     val views = RemoteViews(context.packageName, R.layout.s_m_b__p05)
+    mediaPlayer = MediaPlayer.create(context, songId)
+
     val intentClick = Intent()
     intentClick.action = "com.example.smb_p05.SMB_P05"
     intentClick.component = ComponentName(context, SMB_P05::class.java)
@@ -113,6 +156,37 @@ internal fun updateAppWidget(
         intentClick3,
         PendingIntent.FLAG_IMMUTABLE
     )
+    val intentClick4a = Intent()
+    intentClick4a.putExtra("id", appWidgetId.toString())
+    intentClick4a.action = "com.example.smb_p05.SMB_P05_music_playPause"
+    intentClick4a.component = ComponentName(context, SMB_P05::class.java)
+    val pendingIntent4a = PendingIntent.getBroadcast(
+        context,
+        4,
+        intentClick4a,
+        PendingIntent.FLAG_IMMUTABLE
+    )
+    val intentClick4b = Intent()
+    intentClick4b.action = "com.example.smb_p05.SMB_P05_music_next"
+    intentClick4b.component = ComponentName(context, SMB_P05::class.java)
+    val pendingIntent4b = PendingIntent.getBroadcast(
+        context,
+        5,
+        intentClick4b,
+        PendingIntent.FLAG_IMMUTABLE
+    )
+    val intentClick4c = Intent()
+    intentClick4c.action = "com.example.smb_p05.SMB_P05_music_stop"
+    intentClick4c.component = ComponentName(context, SMB_P05::class.java)
+    val pendingIntent4c = PendingIntent.getBroadcast(
+        context,
+        6,
+        intentClick4c,
+        PendingIntent.FLAG_IMMUTABLE
+    )
+    views.setOnClickPendingIntent(R.id.playPause, pendingIntent4a)
+    views.setOnClickPendingIntent(R.id.next, pendingIntent4b)
+    views.setOnClickPendingIntent(R.id.stop, pendingIntent4c)
     views.setOnClickPendingIntent(R.id.openIntent, pendingIntent3)
     views.setOnClickPendingIntent(R.id.openWebsiteButton, pendingIntent)
     views.setOnClickPendingIntent(R.id.image, pendingIntent2)
